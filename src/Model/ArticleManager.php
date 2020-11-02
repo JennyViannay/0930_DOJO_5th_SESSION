55d50a32e8c1e117544a1cde5d41e91c77578431
JennyViannay <jenny.viannay75@gmail.com>
2020-11-03 00:13:12 +0100

    add correction (code to clean)

--- src/Model/ArticleManager.php
+++ src/Model/ArticleManager.php
@@ -34,7 +34,6 @@ class ArticleManager extends AbstractManager
         ')->fetchAll();
 
         $images = $this->pdo->query('SELECT url, article_id FROM image')->fetchAll();
-
         $result = [];
         foreach ($articles as $article) {
             $statementImg = $this->pdo->prepare('SELECT url FROM image WHERE article_id=:article_id');
@@ -42,9 +41,10 @@ class ArticleManager extends AbstractManager
             $statementImg->execute();
             $images = $statementImg->fetchAll();
             $article['images'] = $images;
+            array_push($result, $article);
         }
         
-        return $articles;
+        return $result;
     }
 
     public function selectOneById(int $id)
@@ -73,15 +73,6 @@ class ArticleManager extends AbstractManager
         return $article;
     }
 
-    public function search(string $term): array
-    {
-        $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE model LIKE :search ORDER BY name ASC");
-        $statement->bindValue('search', $term.'%', \PDO::PARAM_STR);
-        $statement->execute();
-        $articles = $statement->fetchAll();
-        return $articles;
-    }
-
     /**
      * @param array $article
      * @return int
@@ -102,10 +93,6 @@ class ArticleManager extends AbstractManager
         }
     }
 
-
-    /**
-     * @param int $id
-     */
     public function delete(int $id): void
     {
         // prepared request
@@ -114,11 +101,6 @@ class ArticleManager extends AbstractManager
         $statement->execute();
     }
 
-
-    /**
-     * @param array $article
-     * @return bool
-     */
     public function update(array $article):bool
     {
 
@@ -133,5 +115,161 @@ class ArticleManager extends AbstractManager
         $statement->bindValue('color_id', $article['color_id'], \PDO::PARAM_INT);
 
         return $statement->execute();
+    }
+
+    public function searchByModel(string $term): array
+    {
+        $statement = $this->pdo->prepare("SELECT
+        art.id, art.brand_id, art.model, art.qty, art.model, art.price, art.size_id, art.color_id, 
+        brand.name as brand_name,
+        color.name as color_name,
+        size.size as size 
+        FROM article as art 
+        JOIN brand ON art.brand_id=brand.id
+        JOIN color ON art.color_id=color.id
+        JOIN size ON art.size_id=size.id
+        WHERE model LIKE :search ORDER BY model ASC");
+        $statement->bindValue('search', $term.'%', \PDO::PARAM_STR);
+        $statement->execute();
+        $articles = $statement->fetchAll();
+
+        $images = $this->pdo->query('SELECT url, article_id FROM image')->fetchAll();
+        $result = [];
+        foreach ($articles as $article) {
+            $statementImg = $this->pdo->prepare('SELECT url FROM image WHERE article_id=:article_id');
+            $statementImg->bindValue('article_id', $article['id'], \PDO::PARAM_INT);
+            $statementImg->execute();
+            $images = $statementImg->fetchAll();
+            $article['images'] = $images;
+            array_push($result, $article);
+        }
+        
+        return $result;
+    }
+
+    public function searchByBrand(int $brand_id): array
+    {
+        $statement = $this->pdo->prepare("SELECT
+        art.id, art.brand_id, art.model, art.qty, art.model, art.price, art.size_id, art.color_id, 
+        brand.name as brand_name,
+        color.name as color_name,
+        size.size as size 
+        FROM article as art 
+        JOIN brand ON art.brand_id=brand.id
+        JOIN color ON art.color_id=color.id
+        JOIN size ON art.size_id=size.id
+        WHERE brand_id = :brand_id ORDER BY model ASC");
+        $statement->bindValue('brand_id', $brand_id, \PDO::PARAM_INT);
+        $statement->execute();
+        $articles = $statement->fetchAll();
+        
+        $images = $this->pdo->query('SELECT url, article_id FROM image')->fetchAll();
+        $result = [];
+        foreach ($articles as $article) {
+            $statementImg = $this->pdo->prepare('SELECT url FROM image WHERE article_id=:article_id');
+            $statementImg->bindValue('article_id', $article['id'], \PDO::PARAM_INT);
+            $statementImg->execute();
+            $images = $statementImg->fetchAll();
+            $article['images'] = $images;
+            array_push($result, $article);
+        }
+        
+        return $result;
+    }
+
+    public function searchByColor(int $color_id): array
+    {
+        $statement = $this->pdo->prepare("SELECT
+        art.id, art.brand_id, art.model, art.qty, art.model, art.price, art.size_id, art.color_id, 
+        brand.name as brand_name,
+        color.name as color_name,
+        size.size as size 
+        FROM article as art 
+        JOIN brand ON art.brand_id=brand.id
+        JOIN color ON art.color_id=color.id
+        JOIN size ON art.size_id=size.id
+        WHERE color_id = :color_id ORDER BY model ASC");
+        $statement->bindValue('color_id', $color_id, \PDO::PARAM_INT);
+        $statement->execute();
+        $articles = $statement->fetchAll();
+        
+        $images = $this->pdo->query('SELECT url, article_id FROM image')->fetchAll();
+        $result = [];
+        foreach ($articles as $article) {
+            $statementImg = $this->pdo->prepare('SELECT url FROM image WHERE article_id=:article_id');
+            $statementImg->bindValue('article_id', $article['id'], \PDO::PARAM_INT);
+            $statementImg->execute();
+            $images = $statementImg->fetchAll();
+            $article['images'] = $images;
+            array_push($result, $article);
+        }
+        
+        return $result;
+    }
+
+    public function searchBySize(int $size_id): array
+    {
+        $statement = $this->pdo->prepare("SELECT 
+        art.id, art.brand_id, art.model, art.qty, art.model, art.price, art.size_id, art.color_id, 
+        brand.name as brand_name,
+        color.name as color_name,
+        size.size as size 
+        FROM article as art 
+        JOIN brand ON art.brand_id=brand.id
+        JOIN color ON art.color_id=color.id
+        JOIN size ON art.size_id=size.id
+        WHERE size_id = :size_id ORDER BY model ASC");
+        $statement->bindValue('size_id', $size_id, \PDO::PARAM_INT);
+        $statement->execute();
+        $articles = $statement->fetchAll();
+        
+        $images = $this->pdo->query('SELECT url, article_id FROM image')->fetchAll();
+        $result = [];
+        foreach ($articles as $article) {
+            $statementImg = $this->pdo->prepare('SELECT url FROM image WHERE article_id=:article_id');
+            $statementImg->bindValue('article_id', $article['id'], \PDO::PARAM_INT);
+            $statementImg->execute();
+            $images = $statementImg->fetchAll();
+            $article['images'] = $images;
+            array_push($result, $article);
+        }
+        
+        return $result;
+    }
+
+    public function searchFull(int $color_id, int $size_id, int $brand_id): array
+    {
+        $statement = $this->pdo->prepare("SELECT
+        art.id, art.brand_id, art.model, art.qty, art.model, art.price, art.size_id, art.color_id, 
+        brand.name as brand_name,
+        color.name as color_name,
+        size.size as size 
+        FROM article as art 
+        JOIN brand ON art.brand_id=brand.id
+        JOIN color ON art.color_id=color.id
+        JOIN size ON art.size_id=size.id 
+        WHERE size_id = :size_id
+        AND brand_id = :brand_id
+        AND color_id = :color_id
+        ORDER BY model ASC
+        ");
+        $statement->bindValue('size_id', $size_id, \PDO::PARAM_INT);
+        $statement->bindValue('color_id', $color_id, \PDO::PARAM_INT);
+        $statement->bindValue('brand_id', $brand_id, \PDO::PARAM_INT);
+        $statement->execute();
+        $articles = $statement->fetchAll();
+        
+        $images = $this->pdo->query('SELECT url, article_id FROM image')->fetchAll();
+        $result = [];
+        foreach ($articles as $article) {
+            $statementImg = $this->pdo->prepare('SELECT url FROM image WHERE article_id=:article_id');
+            $statementImg->bindValue('article_id', $article['id'], \PDO::PARAM_INT);
+            $statementImg->execute();
+            $images = $statementImg->fetchAll();
+            $article['images'] = $images;
+            array_push($result, $article);
+        }
+        
+        return $result;
     }
 }
